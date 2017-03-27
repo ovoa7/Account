4c43321225e1358890c34af81ce4fd3333e003bc
KAU <KAU@KAU-PC>
2017-03-27 14:03:19 +0900

    3월 27일 실습2번째 수정

--- 3.13/src/Accounttest.java
+++ 3.13/src/Accounttest.java
@@ -11,7 +11,7 @@ public class Accounttest {
 		double credit_limit=input.nextDouble();
 		System.out.print("대출이자율 입력해주세요");
 		double loan_interest=input.nextDouble();
-		account account = new account(balance);
+		//account account = new account(balance);
 		checkingAccount ckAccount= new checkingAccount(balance,credit_limit,loan_interest,interest);
 		while(true){
 			System.out.print("1.입금, 2.출금  3.조회  4.잔액수정 5.다음달 잔액변동");
@@ -19,25 +19,25 @@ public class Accounttest {
 			if (i==2){
 				System.out.print("얼마?");
 				double debit=input.nextDouble();
-				account.subtract(debit);
+				ckAccount.subtract(debit);
 			}
 			else if(i==1){
 				System.out.print("얼마?");
 				double credit=input.nextDouble();
-				account.add(credit);
+				ckAccount.add(credit);
 			}
 			else if(i==4){
 				System.out.print("수정할 잔액입력");
 				balance=input.nextDouble();
-				account.SetBalance(balance);
+				ckAccount.SetBalance(balance);
 				
 			}
 			else if(i==5){
 				System.out.print("next month");
-				System.out.println(checkingAccount.nextMonth());
+				System.out.println(ckAccount.nextMonth());
 			}
 			else{
-				System.out.println(account.getBalance());
+				System.out.println(ckAccount.getBalance());
 			}
 	
 			
