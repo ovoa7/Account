a9757dd85bed2a661f75c3279bd8547026008c15
KAU <KAU@KAU-PC>
2017-05-08 16:41:19 +0900

    5월8필 Arraylist를 이용하여 sumForAccount method를 추가하였다.

--- 3.13/src/Account.java
+++ 3.13/src/Account.java
@@ -1,12 +1,24 @@
 import java.util.Scanner;
-
+import java.util.ArrayList;
+import java.util.Iterator;
 public abstract class Account implements Valuable{
 	protected double balance;
 	private double time;
 	public Account(double balance){
 		this.balance=balance;
 	}
-	
+	public static double sumForAccount(ArrayList<Account> list){
+		double sum=0;
+		for(Account account:list){
+			sum+=account.getBalance();
+		}
+		return sum;
+	}
+	public static void passTimeForList(ArrayList<Account> list,int month){
+		for(Account account: list){
+			account.passTime(month);
+		}
+	}
 	
 	public void credit(double credit){
 		balance=balance+credit;
